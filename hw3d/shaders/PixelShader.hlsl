a364bdf091b299a6aa012e758275065b22a7aca7
Ryan Beesley <rbeesley@users.noreply.github.com>
2024-10-17 13:53:40 -0700

    Added an index buffer for drawing the triangles. Tris are now colored at their vertices, rendering a colored polygon.

--- hw3d/shaders/PixelShader.hlsl
+++ hw3d/shaders/PixelShader.hlsl
@@ -1,4 +1,10 @@
-float4 main() : SV_TARGET
+struct ps_in
 {
-	return float4(1.0f, 1.0f, 1.0f, 1.0f);
+    float4 pos : SV_POSITION;
+    float4 color : COLOR;
+};
+
+float4 main(ps_in pin) : SV_TARGET
+{
+	return pin.color;
 }
